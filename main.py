88a479ffe3a216eda4d9d92196e0fba2e4c3e1ce
marklarson07 <54913917+marklarson07@users.noreply.github.com>
2019-09-06 16:41:01 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pandas as pandas
 from flask import Flask, render_template, request, url_for
 from joblib import load
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+plt.rc('font', size=14)
 
 # EDA Packages
 import pandas as pd
@@ -19,8 +25,24 @@ def index():
 
 
 @app.route("/",methods=['POST'])
+
 def MLModel():
+
     model = load('crunchbase_ml_test.joblib')
+    # data manip
+    data = pd.read_csv('MLTestAll.csv', header=0)
+    data.drop(['Total Funding Amount'], axis=1, inplace=True)
+    data.drop(['Number of Lead Investors'], axis=1, inplace=True)
+    data.drop(['Number of Funding Rounds'], axis=1, inplace=True)
+    data.fillna(0, inplace=True)
+    X = data[data.columns[:-1]]
+    y = data[data.columns[-1:]]
+
+    plt.hist(data['Number of Founders'])
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    figure = base64.b64encode(buffer.getbuffer()).decode('ascii')
+
     if request.method == 'POST':
         number_of_articles_in = request.form['number_of_articles']
         number_of_articles = int(number_of_articles_in)
@@ -33,9 +55,7 @@ def MLModel():
         sample = np.array([(number_of_articles, number_of_founders, number_of_investors, number_of_acquisitions)])
         test_proba = model.predict_proba(sample)[0][1]
 
-
-
-    return render_template('results.html', prediction="{0:.0f}%".format(test_proba * 100))
+    return render_template('results.html', prediction="{0:.0f}%".format(test_proba * 100),figure=figure)
 
 
 if __name__ == '__main__':
